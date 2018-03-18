4c0913e4ecd89ef8582a85e05d26813edc71a771
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2018-03-18 18:32:11 +0700

    - Xây dựng chức năng hiển thị sản phẩm - 2 - hide quantity

--- src/components/items/ItemForm.js
+++ src/components/items/ItemForm.js
@@ -5,14 +5,16 @@ class ItemForm extends Component {
 	render() {
 		var { price, inventory } = this.props;
 
-		var elePrice = <a data-product={1} href="" className="price"> {price} USD </a>;
+		var elePrice 	= <a data-product={1} href="" className="price"> {price} USD </a>;
+		var eleQuantity =  <input name="quantity-product-1" type="number" defaultValue={1} min={1} />;
 		if(!inventory) {
-			elePrice = <span className="price"> {price} USD</span>;
+			elePrice 	= <span className="price"> {price} USD</span>;
+			eleQuantity = null;
 		}
-		
+
 		return (
 			<form>
-            	<input name="quantity-product-1" type="number" defaultValue={1} min={1} />
+            	{ eleQuantity }
             	{elePrice}
             </form>
 		)
